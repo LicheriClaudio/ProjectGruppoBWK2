789b5ccdbd9c3f2977d2ce35516af88d90a44635
craigbauer89 <craigbauer89@gmail.com>
2022-06-15 14:00:39 +0200

    Craig Update

--- client/js/script.js
+++ client/js/script.js
@@ -74,10 +74,26 @@ function getAllUsers() {
         cardD.className = 'card-body';
         cardB.append(cardD);
 
+        fetch(APIphoto)
+        .then((response) => response.json())
+        .then(photoArr => {
+          photoArr.forEach(photo => {
+            if (photo.id === user.id) {
+
+        let carImg = document.createElement('div');
+        carImg.className = 'cardImg';
+        carImg.innerHTML = `<img src="${photo.thumbnailUrl}">`
+        cardD.append(carImg);
+          }
+          
+        });
+
         let cardName = document.createElement('h5');
         cardName.className = 'card-text';
         cardName.innerText = `${user.name}`;
         cardD.append(cardName);
+        
+
 
         let hr = document.createElement('hr');
         cardD.append(hr);
@@ -96,9 +112,12 @@ function getAllUsers() {
         cardPhone.className = 'card-text';
         cardPhone.innerText = `Tel: ${user.phone}`;
         cardD.append(cardPhone);
-      })
+      });
     });
+});
 }
+
+
 
 
 //---ADDUSERS
@@ -242,4 +261,86 @@ function detailAlbum(id) {
     .then((json) => {
       console.log(json);
     });
+}
+
+
+
+
+
+function  searchUser() {
+  
+
+  let searchItem = document.querySelector('form input');
+  fetch(APIusers)
+  .then((response) => response.json())
+  .then((usersArr) => {
+  
+    /* printTableUser(json); */
+    /* console.log(users); */
+  
+  usersArr.forEach(user => {
+  if (searchItem.value === user.name) {
+    
+
+  let list = document.querySelector('#general');
+            while (list.hasChildNodes()) {  
+                list.removeChild(list.firstChild);
+            }
+          
+  let a = document.querySelector('#general');
+        let cardA = document.createElement('div');
+        cardA.className = 'col';
+        a.appendChild(cardA);
+
+        let cardB = document.createElement('div');
+        cardB.classList = 'card';
+        cardA.append(cardB);
+
+        let cardD = document.createElement('div');
+        cardD.className = 'card-body';
+        cardB.append(cardD);
+
+        fetch(APIphoto)
+        .then((response) => response.json())
+        .then(photoArr => {
+          photoArr.forEach(photo => {
+            if (photo.id === user.id) {
+
+        let carImg = document.createElement('div');
+        carImg.className = 'cardImg';
+        carImg.innerHTML = `<img src="${photo.thumbnailUrl}">`
+        cardD.append(carImg);
+          }
+          
+        });
+
+        let cardName = document.createElement('h5');
+        cardName.className = 'card-text';
+        cardName.innerText = `${user.name}`;
+        cardD.append(cardName);
+    
+
+
+        let hr = document.createElement('hr');
+        cardD.append(hr);
+
+        let cardAddress = document.createElement('p');
+        cardAddress.className = 'card-text';
+        cardAddress.innerText = `${user.address.street}, ${user.address.suite} (${user.address.city})`;
+        cardD.append(cardAddress);
+
+        let cardEmail = document.createElement('p');
+        cardEmail.className = 'card-text';
+        cardEmail.innerText = `${user.email}`;
+        cardD.append(cardEmail);
+
+        let cardPhone = document.createElement('p');
+        cardPhone.className = 'card-text';
+        cardPhone.innerText = `Tel: ${user.phone}`;
+        cardD.append(cardPhone);
+      });
+    }
+    });
+  
+});
 }
