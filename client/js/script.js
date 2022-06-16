797a0321a7a9bf4a4a3a943c3bbf213ae80cc252
craigbauer89 <craigbauer89@gmail.com>
2022-06-16 16:19:08 +0200

    MyPage Addition

--- client/js/script.js
+++ client/js/script.js
@@ -282,7 +282,7 @@ function login() {
           document.getElementById("form3Example3").value === mail.email &&
           document.getElementById("form3Example4").value == "qwerty"
         ) {
-          window.location = "http://127.0.0.1:5500/client/Project.html";
+          window.location = "http://127.0.0.1:5500/client/MyPage.html";
           alert("benvenuto!");
           console.log(mail);
           console.log(password);
